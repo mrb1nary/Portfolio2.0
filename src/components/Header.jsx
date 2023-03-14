d5bcee08fce2c8c89864baeec4f8415033bc4e9e
Anubhab <anubhab.techie@gmail.com>
2023-03-14 17:10:12 +0530

    Depracated previous design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,32 @@
 import React from "react";
+import wave from "../assets/wave-haikei.svg";
+import profile from "../assets/profile.jpg";
 
 function Header() {
   return (
-    <div className="flex w-[100%] sticky justify-center items-center">
-      <ul className="flex w-[100%] sticky justify-center items-center">
-        <li className="m-5">About Me</li>
-        <li className="m-5">Projects</li>
-        <li className="m-5">Contact</li>
-      </ul>
-      <button className="p-2 rounded-md items-center justify-center mr-5 bg-teal-300">Resume</button>
+    <div>
+      <img
+        src={wave}
+        alt="wave"
+        className="absolute z-[-10] h-[100vh] w-full object-cover"
+      />
+
+      <navbar className="flex w-full justify-center items-center text-white">
+        <ul className="flex justify-between items-center w-[50%] mt-6">
+          <li>Contact</li>
+          <li>Projects</li>
+          <li>Resume</li>
+        </ul>
+      </navbar>
+      <div className="flex justify-center items-center gap-10 h-[800px]">
+        <div className=" text-white flex justify-center items-center">
+          <h3>Hi, I'm Anubhab!</h3>
+        </div>
+        <div className="bg-[#8728d8] overflow-clip relative opacity-30 rounded-2xl w-[600px] h-[450px] ">
+          <div className=" h-[150px] w-[150px] absolute rounded-full m-[5px] blur-2xl bg-pink-400 "></div>
+          <div className=" h-[150px] w-[150px] absolute rounded-full top-[-120] bottom-0 right-10 blur-2xl bg-violet-400 "></div>
+        </div>
+      </div>
     </div>
   );
 }
